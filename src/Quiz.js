3b5d628877c49bec9985b083299bd0de52658c5a
akader1 <33326886+akader1@users.noreply.github.com>
2020-08-25 00:31:38 -0400

    Solution

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React, {Component} from 'react'
-
-let quizData = require('./quiz_data.json')
-
-class Quiz extends Component{
-  constructor(props){
-    super(props){
-      this.state= {quiz.position: 1 }
-    }
-  }
-
-  render(){
-    return(
-      <div><div className="QuizQuestion">{quizData.quiz_questions[0]
-        .instruction_text/div> </div>
-
-    )
-
-    
-  }
-}
-
-export default Quiz
-=======
 import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
@@ -42,4 +17,3 @@ class Quiz extends Component {
 }
 
 export default Quiz
->>>>>>> 37462b6bb73bc7fd1a69c8bce537b4ad294986dd
