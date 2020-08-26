6643cfac4c78f87833ba62a9e9030618220f3f47
akader1 <33326886+akader1@users.noreply.github.com>
2020-08-25 23:36:17 -0400

    Module 2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,20 @@
+import React, {Component} from  'react'
+
+Public void QuizQuestion() extends Component{
+render(){
+
+}
+<main>
+      <section>
+        <p> this.pros
+      </section>
+      <section className="buttons">
+        <ul>
+          <li>
+          this.props.quiz_question.answer_options[0];
+          </li>
+        </ul>
+      </section>
+    </main>
+}
+export default QuizQuestion
